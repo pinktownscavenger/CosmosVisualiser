6d35294170a5d31c36b9b7ee48436b766d613254
bilal <bilalmalik1548@gmail.com>
2024-09-11 18:30:33 +0500

    Pushing

---
+++ proxy-server.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const gremlin = require('gremlin');
+const { DriverRemoteConnection, Graph } = gremlin.structure;
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+// Allow requests from the specific origin
+const allowedOrigin = 'http://192.168.56.1:3000'; // Replace with your frontend's URL
+
+app.use(cors({
+  origin: allowedOrigin,
+  credentials: true // Enable credentials
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+function mapPropertiesToObj(properties) {
+  const obj = {};
+  for (const key in properties) {
+    obj[key] = properties[key].map(item => item.value);
+  }
+  return obj;
+}
+
+function edgesToJson(edges) {
+  return edges.map(edge => ({
+    id: typeof edge.id !== "string" ? JSON.stringify(edge.id) : edge.id,
+    from: edge.from,
+    to: edge.to,
+    label: edge.label,
+    properties: edge.properties ? mapPropertiesToObj(edge.properties) : {}
+  }));
+}
+
+function nodesToJson(nodeList, edgeList) {
+    // Create a map to quickly find edges connected to each node
+    const edgeMap = {};
+    edgeList.forEach(edge => {
+      if (!edgeMap[edge.from]) {
+        edgeMap[edge.from] = [];
+      }
+      edgeMap[edge.from].push(edge);      
+    });
+
+  return nodeList.map(node => {
+    const nodeEdges = edgeMap[node.id] || [];
+    const formattedEdges = edgesToJson(nodeEdges);
+
+    // console.log('Node:', JSON.stringify({
+    //   id: node.id,
+    //   label: node.label,
+    //   type: node.type,
+    //   properties: mapPropertiesToObj(node.properties),
+    //   edges: formattedEdges
+    // }, null, 2));
+
+    // return {
+    //   id: node.id,
+    //   label: node.label,
+    //   type: node.type,
+    //   properties: mapPropertiesToObj(node.properties),
+    //   edges: edgesToJson(edgeMap[node.id] || [])
+    // };
+
+    return {
+      id: node.id,
+      label: node.label,
+      type: node.type,
+      properties: mapPropertiesToObj(node.properties), // convert properties to a plain object
+      edges: formattedEdges // Use edgesToJson to convert connected edges
+    };
+  });
+}
+
+function makeEdgeQuery(nodeLimit) {
+  const nodeLimitQuery = !isNaN(nodeLimit) && Number(nodeLimit) > 0 ? `.limit(${nodeLimit})` : '';
+  return `
+    g.E()${nodeLimitQuery}
+      .project('id', 'label', 'from', 'to', 'properties')
+      .by(id())
+      .by(label())
+      .by(outV().id()) // Extracting the 'from' vertex ID
+      .by(inV().id())  // Extracting the 'to' vertex ID
+      .by(valueMap())  // Extracting properties of the edge
+  `;
+}
+
+function makeVertexQuery(query, nodeLimit) {  
+  const nodeLimitQuery = !isNaN(nodeLimit) && Number(nodeLimit) > 0 ? `.limit(${nodeLimit})` : '';
+  // THIS QUERY DOES NOT RETURN EDGES
+  return `${query}${nodeLimitQuery}`;
+}
+
+app.post('/query', async (req, res, next) => {
+  const endpoint = "wss://trialvm.gremlin.cosmos.azure.com:443/";
+  const primaryKey = "VDVZgD1PpmmsTm0F3RL9jur8zm08vREuq2nuGHpSaPYy05kX5yLOXx6pypBnkz1PEyzc4V23TSM9ACDbxXIoXQ==";
+  const database = 'trialdb';
+  const container = 'ontologyGraph';
+
+  const authenticator = new gremlin.driver.auth.PlainTextSaslAuthenticator(`/dbs/${database}/colls/${container}`, primaryKey);
+
+  const client = new gremlin.driver.Client(endpoint, {
+    authenticator,
+    traversalSource: 'g',
+    mimeType: 'application/vnd.gremlin-v2.0+json',
+    rejectUnauthorized: true,
+  });
+
+  const nodeLimit = req.body.nodeLimit;
+  const query = req.body.query;
+
+  client.submit(makeVertexQuery(query, nodeLimit), {})
+  .then((vertexResult) => {
+    // console.log('Received result:', vertexResult);
+    client.submit(makeEdgeQuery(nodeLimit))
+      .then(edgeResult => {
+        // console.log('Received result:', edgeResult);
+        const vertices = nodesToJson(vertexResult._items, edgeResult._items);
+        // console.log('Combined Data:', JSON.stringify(vertices, null, 2));
+        res.send(vertices);
+      })
+      .catch(edgeErr => {
+        console.error('Error fetching edges:', edgeErr);
+        res.status(500).send({ error: 'Failed to fetch edges' });
+      });
+  })
+  .catch((vertexErr) => {
+    console.error('Error fetching vertices:', vertexErr);
+    res.status(500).send({ error: 'Failed to fetch vertices' });
+  });
+
+});
+
+app.listen(port, () => console.log(`Simple gremlin-proxy server listening on port ${port}!`));
---
+++ src/logics/utils.js
@@ -0,0 +1,55 @@
+import _ from 'lodash';
+
+const selectRandomField = (obj) => {
+  let firstKey;
+  for (firstKey in obj) break;
+  return firstKey;
+};
+
+export const getDiffNodes = (newList, oldList) => {
+  return _.differenceBy(newList, oldList, (node) => node.id);
+};
+
+export const getDiffEdges = (newList, oldList) => {
+  return _.differenceBy(newList, oldList, (edge) => `${edge.from},${edge.to}`);
+};
+
+export const extractEdgesAndNodes = (nodeList, nodeLabels=[]) => {
+  let edges = [];
+  const nodes = [];
+
+  console.log('Received nodeList:', nodeList); // new
+
+  const nodeLabelMap =_.mapValues( _.keyBy(nodeLabels, 'type'), 'field');
+
+  _.forEach(nodeList, (node) => {
+    const type = node.label;
+    if (!nodeLabelMap[type]) {
+      const field = selectRandomField(node.properties);
+      const nodeLabel = { type, field };
+      nodeLabels.push(nodeLabel);
+      nodeLabelMap[type] = field;
+    }
+    const labelField = nodeLabelMap[type];
+    const label = labelField in node.properties ? node.properties[labelField] : type;
+    nodes.push({ id: node.id, label: String(label), group: node.label, properties: node.properties, type });
+
+    console.log('Edges from node:', node.id, node.edges); // new
+
+    edges = edges.concat(_.map(node.edges, edge => ({ ...edge, type: edge.label, arrows: { to: { enabled: true, scaleFactor: 0.5 } } })));
+  });
+
+  return { edges, nodes, nodeLabels }
+};
+
+export const findNodeById = (nodeList, id) => {
+  return _.find(nodeList, node => node.id === id);
+};
+
+export const stringifyObjectValues = (obj) => {
+  _.forOwn(obj, (value, key) => {
+    if (!_.isString(value)) {
+      obj[key] = JSON.stringify(value);
+    }
+  });
+};
